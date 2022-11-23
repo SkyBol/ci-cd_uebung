cd60513ae47ec7e02f079169eda1efdbd24059f8
SkyBollin <sky.bollin@noseryoung.ch>
2022-11-23 15:14:54 +0100

    sonar qube

--- build/generated/sources/annotationProcessor/java/main/com/example/demo/domain/blogpost/dto/BlogPostMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/com/example/demo/domain/blogpost/dto/BlogPostMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-11-23T11:55:57+0100",
+    date = "2022-11-23T14:15:46+0100",
     comments = "version: 1.5.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.4.jar, environment: Java 18.0.1.1 (Oracle Corporation)"
 )
 @Component
